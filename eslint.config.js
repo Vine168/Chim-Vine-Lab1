9c975cc8fe4d4366df3dafaee79236d9a7377bfb
nasa-earth <your.email@example.com>
2025-11-03 14:59:40 +0700

    change node version

--- eslint.config.js
+++ eslint.config.js
@@ -1,27 +1,27 @@
-const js = require('@eslint/js');
+const js = require("@eslint/js");
 
 module.exports = [
   js.configs.recommended,
   {
     languageOptions: {
       ecmaVersion: 12,
-      sourceType: 'commonjs',
+      sourceType: "commonjs",
       globals: {
-        console: 'readonly',
-        process: 'readonly',
-        require: 'readonly',
-        module: 'readonly',
-        __dirname: 'readonly',
-        __filename: 'readonly',
-        exports: 'readonly',
-        describe: 'readonly',
-        it: 'readonly',
-        before: 'readonly',
-        after: 'readonly',
-        beforeEach: 'readonly',
-        afterEach: 'readonly'
-      }
+        console: "readonly",
+        process: "readonly",
+        require: "readonly",
+        module: "readonly",
+        __dirname: "readonly",
+        __filename: "readonly",
+        exports: "readonly",
+        describe: "readonly",
+        it: "readonly",
+        before: "readonly",
+        after: "readonly",
+        beforeEach: "readonly",
+        afterEach: "readonly",
+      },
     },
-    rules: {}
-  }
+    rules: {},
+  },
 ];
